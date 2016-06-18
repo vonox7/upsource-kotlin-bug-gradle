7af683d0e6fe9bbdd7ea6d75dc98eb3dc334e073
Valentin Slawicek <valentin.slawi@gmail.com>
2016-06-18 11:39:28 +0200

    Add function call

--- src/com/moshbit/upsource/kotlin/bug/Main.kt
+++ src/com/moshbit/upsource/kotlin/bug/Main.kt
@@ -7,5 +7,14 @@ object Main {
 
     val helloWorldWithSpaces = helloWorld.prependIndent(" ".repeat(30)) // "     Hello#World"
     println(helloWorldWithSpaces.replace("#", " ").trim()) // "Hello World"
+
+    @Suppress("DEPRECATION")
+    deprecatedFunction() // "Hello world from a deprecated function"
+  }
+
+  @Suppress("DeprecatedCallableAddReplaceWith")
+  @Deprecated("This function is deprecated")
+  fun deprecatedFunction() {
+    println("Hello world from a deprecated function")
   }
 }
