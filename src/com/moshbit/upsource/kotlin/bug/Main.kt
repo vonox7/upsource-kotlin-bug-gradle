a3c6ccc46350ecfb700f54a177bcc05fab5212d6
Valentin Slawicek <valentin.slawi@gmail.com>
2016-06-18 11:17:29 +0200

    Even more sophisticated hello world

--- src/com/moshbit/upsource/kotlin/bug/Main.kt
+++ src/com/moshbit/upsource/kotlin/bug/Main.kt
@@ -3,7 +3,9 @@ package com.moshbit.upsource.kotlin.bug
 object Main {
   @JvmStatic fun main(args: Array<String>) {
     var helloWorld = "Hello_World"
-    helloWorld = helloWorld.split("_").joinToString(separator = " ")
-    println(helloWorld)
+    helloWorld = helloWorld.split("_").joinToString(separator = "#") // "Hello#World"
+
+    val helloWorldWithSpaces = helloWorld.prependIndent(" ".repeat(30)) // "     Hello#World"
+    println(helloWorldWithSpaces.replace("#", " ").trim()) // "Hello World"
   }
 }
